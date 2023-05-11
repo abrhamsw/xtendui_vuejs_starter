89bd4bb6cb6638d81e241f288b3ff4c33d4f2bbd
abrhamsw <122590205+abrhamsw@users.noreply.github.com>
2023-05-11 23:01:35 +0300

    Done :)

---
+++ README.md
@@ -0,0 +1,9 @@
+## Xtendui - VueJs Starter with Vite
+
+git clone https://github.com/abrhamsw/xtendui_vuejs_with_vite_starter.git
+
+cd xtendui_vuejs_with_vite_starter
+
+npm install
+
+npm run dev
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<!--  -->
+<script setup>
+import Xtendui from './components/Xtendui.vue'
+</script>
+
+<!--  -->
+<template>
+  <!-- For Test    -->
+  <Xtendui class="bg-slate-700 text-white p-8" />
+</template>
+
+<style></style>
